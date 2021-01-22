ad2ad135dd0ad50b396acff47ad298ff84d83f51
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-22 14:18:41 -0800

    Setting up for fundamentals

---
+++ Fundamentals/sum_to_one_digit.py
@@ -0,0 +1,5 @@
+# Sum To One Digit
+# Implement a function sumToOne(num) that,
+# given a number, sums that number’s digits
+# repeatedly until the sum is only one digit. Return
+# that final one digit result.
