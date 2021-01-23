2ee97fc9ae4f6f738127bf5163f4dbafd009fcb3
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-22 22:13:44 -0800

    Solved coin change

--- fundamentals/4_coin_change.js
+++ fundamentals/4_coin_change.js
@@ -3,3 +3,20 @@
 // and computes how to represent that amount with the smallest number of coins.
 // Console.log the result.
 
+function generateCoinChange(cents) {
+    var quarters = Math.floor(cents / 25);
+    cents = cents - (quarters * 25); 
+
+    var dimes = Math.floor(cents / 10);
+    cents = cents - (dimes * 10); 
+
+    var nickels = Math.floor(cents / 5);
+
+    var pennies = cents - (nickels * 5);
+
+    var numCoins = quarters + dimes + nickels + pennies;
+
+    console.log("Number of coins: " + String(numCoins));
+}
+
+generateCoinChange(99);
