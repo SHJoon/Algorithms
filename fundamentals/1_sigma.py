e22e263b41475191666a49eec7efe583904808a5
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-22 21:39:56 -0800

    Solved sigma, factorial

--- fundamentals/1_sigma.py
+++ fundamentals/1_sigma.py
@@ -3,3 +3,14 @@
 # number, returns the sum of all positive integers
 # from 1 up to number (inclusive). Ex.: sigma(3)
 # = 6 (or 1+2+3); sigma(5) = 15 (or 1+2+3+4+5)
+
+def sigma(num):
+    sum = 0
+    for i in range(num + 1):
+        sum += i
+
+    return sum
+
+
+print(sigma(3))
+print(sigma(5))
--- fundamentals/2_factorial.py
+++
@@ -1,6 +0,0 @@
-# Factorial
-# Write a function factorial(num) that, given a
-# number, returns the product (multiplication) of all
-# positive integers from 1 up to number (inclusive).
-# For example, factorial(3) = 6 (or 1 * 2 * 3);
-# factorial(5) = 120 (or 1 * 2 * 3 * 4 * 5).
