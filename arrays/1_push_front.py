feabf553a4e68d1241f1b01a9b81d4f632d7aba3
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-22 16:17:25 -0800

    Adding more algo problems

---
+++ arrays/1_push_front.py
@@ -0,0 +1,4 @@
+# PushFront
+# Given an array and an additional value, insert this
+# value at the beginning of the array. Do this
+# without using any built-in array methods.
---
+++ fundamentals/7_fibonacci.py
@@ -0,0 +1,6 @@
+# Fibonacci
+# Implement the Fibonacci function, a famous mathematical equation that
+# generates a numerical sequence such that each number is the sum of the
+# previous two. The Fibonacci numbers at index 0 and 1, coincidentally, have
+# values of 0 and 1. Your function should accept an argument of which Fibonacci number.
+# Examples: fibonacci(2) = 1, fibonacci(3) = 2, fibonacci(4) = 3, fibonacci(5) = 5, etc.
