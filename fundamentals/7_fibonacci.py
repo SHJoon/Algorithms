f3976560b32570de28a2d2c23da74969bbd78745
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-24 10:23:01 -0800

    Solved fib

--- fundamentals/7_fibonacci.py
+++ fundamentals/7_fibonacci.py
@@ -4,3 +4,23 @@
 # previous two. The Fibonacci numbers at index 0 and 1, coincidentally, have
 # values of 0 and 1. Your function should accept an argument of which Fibonacci number.
 # Examples: fibonacci(2) = 1, fibonacci(3) = 2, fibonacci(4) = 3, fibonacci(5) = 5, etc.
+
+def fibonacci(num):
+    if num in (0,1):
+        return num
+    
+    first = 0
+    second = 1
+    count = 0
+
+    while count < num:
+        temp = second
+        second = first + second
+        first = temp
+        count += 1
+    
+    return first
+
+print(fibonacci(4))
+print(fibonacci(5))
+print(fibonacci(6))
