70ec3564812ec810992ccf6a50f0c847e1913dcc
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-23 23:28:17 -0800

    Solved sum to digit

--- fundamentals/6_sum_to_one_digit.py
+++ fundamentals/6_sum_to_one_digit.py
@@ -1,5 +1,16 @@
 # Sum To One Digit
 # Implement a function sumToOne(num) that,
 # given a number, sums that number’s digits
-# repeatedly until the sum is only one digit. Return
-# that final one digit result.
+# repeatedly until the sum is only one digit.
+# Return that final one digit result.
+
+def sumToOne(num):
+    while(len(str(num)) > 1):
+        sum = 0
+        for digit in str(num):
+            sum += int(digit)
+        num = sum
+    
+    return sum
+
+print(sumToOne(1234567))
