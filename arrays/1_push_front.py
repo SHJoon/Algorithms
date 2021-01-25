410711b3b14dc53efa73c4b3cebee5223a1df9e5
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-25 13:20:23 -0800

    Finished push front algo

--- arrays/1_push_front.py
+++ arrays/1_push_front.py
@@ -2,3 +2,13 @@
 # Given an array and an additional value, insert this
 # value at the beginning of the array. Do this
 # without using any built-in array methods.
+
+def pushFront(lst, val):
+    lst.append(lst[len(lst) - 1])
+    for i in range(len(lst) - 1, 0, -1):
+        lst[i] = lst[i - 1]
+    lst[0] = val
+
+my_list = [0,1,2]
+pushFront(my_list, 0)
+print(my_list)
