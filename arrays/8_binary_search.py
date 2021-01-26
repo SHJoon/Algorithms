05f6d8d2c20e2388932ce8919e4dd46ab6eae453
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-26 02:27:15 -0800

    Finished binary search

--- arrays/8_binary_search.py
+++ arrays/8_binary_search.py
@@ -4,3 +4,22 @@
 # sequentially iterate the entire array. Instead,
 # ‘divide and conquer’, taking advantage of the fact
 # that the array is sorted.
+
+def binary_search(sorted_list, num):
+    start = 0
+    end = len(sorted_list) - 1
+    while start < end:
+        mid = (start + end) // 2
+        if num > sorted_list[mid]:
+            start = mid + 1
+        elif num < sorted_list[mid]:
+            end = mid - 1
+        else:
+            return True
+    return False
+
+my_list = [0,1,5,7,10,15,50,62,98]
+print(binary_search(my_list, 4))
+print(binary_search(my_list, 10))
+print(binary_search(my_list, 62))
+print(binary_search(my_list, 90))
