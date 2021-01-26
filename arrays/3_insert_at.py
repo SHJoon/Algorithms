9dbd4f7acf341783da3924529058da7296b8715a
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-25 23:07:22 -0800

    Solved insertAt

--- arrays/3_insert_at.py
+++ arrays/3_insert_at.py
@@ -4,3 +4,13 @@
 # without using built-in array methods. You can
 # think of PushFront(arr,val) as equivalent to
 # InsertAt(arr,0,val).
+
+def insert_at(lst, idx, val):
+    lst.append(0)
+    for i in reversed(range(idx, len(lst))):
+        lst[i] = lst[i - 1]
+    lst[idx] = val
+
+my_lst = [0,1,3]
+insert_at(my_lst, 2, 2)
+print(my_lst)
