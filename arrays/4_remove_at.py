bc83a616067986b9115358560e54722ce0c075c2
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-25 23:12:10 -0800

    Finished Remove At

--- arrays/4_remove_at.py
+++ arrays/4_remove_at.py
@@ -4,3 +4,13 @@
 # Do this without using any built-in array methods
 # except pop(). Think of PopFront(arr) as
 # equivalent to RemoveAt(arr,0).
+
+def remove_at(lst, idx):
+    for i in range(idx, len(lst) - 1):
+        lst[i] = lst[i + 1]
+    lst.pop()
+
+my_lst = [0,1,2,3]
+
+remove_at(my_lst, 2)
+print(my_lst)
