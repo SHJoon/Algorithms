91fda8d44cedd2d402ce7e656420f92c0bb48ecc
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-26 00:38:54 -0800

    Solved skyline heights

--- arrays/7_skyline_heights.py
+++ arrays/7_skyline_heights.py
@@ -5,3 +5,18 @@
 # high (but hidden behind the seven-story onbe). You are situated at street level.
 # Return an array containing heights of the buildings you can see, in order.
 # Given [1,-1,7,3] return [1,7].
+
+def skyline_heights(lst):
+    highest_floor = lst[0]
+    visible = [highest_floor]
+    
+    for floor in lst:
+        if floor > highest_floor:
+            visible.append(floor)
+            highest_floor = floor
+
+    return visible
+
+print(skyline_heights([1,-1,7,3]))
+print(skyline_heights([1,5,7,-6]))
+print(skyline_heights([-1,8,7,3]))
