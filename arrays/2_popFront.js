be308b84ff1702925fde836efaca47e84c116a7d
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-01-25 22:33:01 -0800

    Finished popFront

--- arrays/2_popFront.js
+++ arrays/2_popFront.js
@@ -2,3 +2,14 @@
 // Given an array, remove and return the value at
 // the beginning of the array. Do this without using
 // any built-in array methods except pop().
+
+function popFront(arr){
+    for(var i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    arr.pop();
+}
+
+myArr = [0,1,2,3];
+popFront(myArr);
+console.log(myArr);
